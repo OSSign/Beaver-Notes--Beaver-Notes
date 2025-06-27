89ec520aa456f7eb952340e81035c747c081f320
Lars Scheibling <24367830+scheibling@users.noreply.github.com>
2025-06-27 16:02:05 +0200

    Fix signtool

--- electron-builder.config.cjs
+++ electron-builder.config.cjs
@@ -1,5 +1,22 @@
 const packageJSON = require('./package.json');
 
+const signtool = async function(configuration) {
+  const AST_CERT = process.env.AST_CERT;
+  const AST_IDENT = process.env.AST_IDENT;
+  const AST_SECRET = process.env.AST_SECRET;
+  const AST_TD = process.env.AST_TD;
+  const AST_TENANT = process.env.AST_TENANT;
+  const AST_TIMESTAMP = process.env.AST_TIMESTAMP;
+  const AST_VAULT = process.env.AST_VAULT;
+
+  require("child_process").execSync(
+    `AzureSignTool.exe sign -kvu "${AST_VAULT}" -kvc "${AST_CERT}" -kvi "${AST_IDENT}" -kvs "${AST_SECRET}" --azure-key-vault-tenant-id "${AST_TENANT}" -tr "${AST_TIMESTAMP}" -td ${AST_TD} "${configuration.path}"`,
+    {
+      stdio: "inherit"
+    }
+  );
+};
+
 /**
  * @type {import('electron-builder').Configuration}
  * @see https://www.electron.build/configuration/configuration
@@ -82,9 +99,7 @@ const electronBuilderConfig = {
         arch: ['x64', 'arm64']
       },
     ],
-    //signtoolOptions: {
-    sign: "file://./sign.js"
-    //}
+    sign: signtool
   },
   nsis: {
     oneClick: true,
